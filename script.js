79c186d73c2b5aa55f3cfa09a9babb724e9d310f
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-03-19 17:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const yourArray = ["one", "two", 1, true, false, 2, 1];
+//const yourArray = ["one", "two", 1, true, false, 2, 1];
 //console.log(yourArray.length);
 
+let yourArray=['one', 2, 'three', true, false];
